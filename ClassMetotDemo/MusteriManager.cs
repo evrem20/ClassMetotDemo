e6df64144c1f9057bb198a59edf46b8d94288fbd
merve çalışır <m-r-v-c-96@hotmail.com>
2021-01-21 20:46:18 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Hoşgeldiniz Sayın" + " " + musteri.MusteriAdi + " " +musteri.MusteriSoyadi + " " +musteri.MusteriYasi );
+        }
+
+        
+        
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Bülent";
+            musteri1.MusteriSoyadi = "Kara";
+            musteri1.MusteriYasi = 50;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Mehmet";
+            musteri2.MusteriSoyadi = "Eren";
+            musteri2.MusteriYasi = 39;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.MusteriAdi = "Hatice";
+            musteri3.MusteriSoyadi = "Yeşil";
+            musteri3.MusteriYasi = 35;
+
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+
+
+
+
+
+
+        }
+    }
+}
